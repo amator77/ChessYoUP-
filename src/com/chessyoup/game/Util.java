66e8c1a671c575130188e5c6b60ecc53fc3852c2
florealeonard <leonard.florea@woowteam.com>
2014-02-06 18:19:27 +0200

    small changes

--- src/com/chessyoup/game/Util.java
+++ src/com/chessyoup/game/Util.java
@@ -4,6 +4,8 @@ import org.goochjs.glicko2.Rating;
 import org.goochjs.glicko2.RatingCalculator;
 import org.goochjs.glicko2.RatingPeriodResults;
 
+import com.chessyoup.model.TimeControl;
+
 public class Util {
 	
 	public static RatingCalculator ratingSystem = new RatingCalculator(0.06, 0.5);
@@ -18,5 +20,16 @@ public class Util {
 		RatingPeriodResults results = new RatingPeriodResults();
 		results.addDraw(player1, player2);
 		ratingSystem.updateRatings(results);
+	}
+	
+	public int getGameVariant(int time , int increment,boolean rated){
+		StringBuffer sb = new StringBuffer(String.valueOf(time/1000));
+		sb.append(String.valueOf(increment/1000));
+		sb.append(rated ? "1" : "0");		
+		return Integer.parseInt(sb.toString());
+	}
+	
+	public TimeControl getTimeControll(int variant){			
+		return null;
 	}
 }
